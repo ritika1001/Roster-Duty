f8ce55ae42ad5bb35ae68522ba5dea7d6bce43d2
ritika1001 <72311548+ritika1001@users.noreply.github.com>
2020-10-03 21:42:22 +0530

    Edited sensitive data

--- admin_destination.php
+++ admin_destination.php
@@ -47,7 +47,7 @@ try {
     $mail->Host = 'smtp.gmail.com';  
     $mail->SMTPAuth = true;         
     $mail->Username = 'richabelair04@gmail.com';         
-    $mail->Password = 'richa0404';                       
+    $mail->Password = '*******';                       
     $mail->SMTPSecure = 'tls';                           
     $mail->Port = 587;                                   
     
